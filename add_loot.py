0f05926ad4c3f2f22e02b48c540a35fd498384e7
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-07 15:05:21 -0500

    Add main boilerplate.

--- add_loot.py
+++ add_loot.py
@@ -13,15 +13,6 @@ logging.basicConfig(
 logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 
-def add_to_inventory(inventory, added_items):
-    for i, _ in enumerate(added_items):
-        if added_items[i] in inventory.keys():
-            inventory[added_items[i]] = inventory.get(added_items[i], 0) + 1
-        else:
-            inventory.setdefault(str(added_items[i]), 1)
-    return inventory
-
-
 starting_inventory_1 = {"gold coin": 42, "rope": 1}
 starting_inventory_2 = {"rope": 2, "gold coin": 1337}
 starting_inventory_3 = {"vorpal sword": 1, "rope": 3, "gold coin": 42}
@@ -30,5 +21,20 @@ dragon_loot_1 = ["gold coin", "dagger", "gold coin", "gold coin", "ruby"]
 dragon_loot_2 = ["dagger", "ruby", "gold coin", "gold coin", "ruby"]
 dragon_loot_3 = ["broken dagger", "gold coin", "ruby", "vorpal sword"]
 
-final_inventory = add_to_inventory(starting_inventory_1, dragon_loot_2)
-display_inventory(final_inventory)
+
+def add_to_inventory(inventory, loot):
+    for i, _ in enumerate(loot):
+        if loot[i] in inventory.keys():
+            inventory[loot[i]] = inventory.get(loot[i], 0) + 1
+        else:
+            inventory.setdefault(str(loot[i]), 1)
+    return inventory
+
+
+def main():
+    final_inventory = add_to_inventory(starting_inventory_1, dragon_loot_2)
+    display_inventory(final_inventory)
+
+
+if __name__ == "__main__":
+    main()
