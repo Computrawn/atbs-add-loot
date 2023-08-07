15e1d3074626c731dd3f1f5727d7ce70e77d952b
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-07 14:58:05 -0500

    Bring up to standards.

--- add_loot.py
+++ add_loot.py
@@ -1,35 +1,34 @@
-# List to Dictionary Function for Fantasy Game Inventory
+#!/usr/bin/env python3
+# filename.py — Short description of project goes here.
+# For more information, see README.md
+
+from inventory import display_inventory
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="logging.txt",
+    format="%(asctime)s -  %(levelname)s -  %(message)s",
+)
+logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 
-def addToInventory(inventory, addedItems):
-    for i in range(len(addedItems)):
-        if addedItems[i] in inventory.keys():
-            inventory[addedItems[i]] = inventory.get(addedItems[i], 0) + 1
+def add_to_inventory(inventory, added_items):
+    for i, _ in enumerate(added_items):
+        if added_items[i] in inventory.keys():
+            inventory[added_items[i]] = inventory.get(added_items[i], 0) + 1
         else:
-            inventory.setdefault(str(addedItems[i]), 1)
+            inventory.setdefault(str(added_items[i]), 1)
     return inventory
 
 
-def displayInventory(inventory):
-    print("Inventory:")
-    item_total = 0
-    for k, v in sorted(inventory.items()):
-        if k != "dagger":
-            print(f"{v} {k}")
-    for k, v in inventory.items():
-        if k == "dagger":
-            print(f"{v} {k}")
-        item_total += v
-    print(f"Total number of items: {item_total}")
+starting_inventory_1 = {"gold coin": 42, "rope": 1}
+starting_inventory_2 = {"rope": 2, "gold coin": 1337}
+starting_inventory_3 = {"vorpal sword": 1, "rope": 3, "gold coin": 42}
 
+dragon_loot_1 = ["gold coin", "dagger", "gold coin", "gold coin", "ruby"]
+dragon_loot_2 = ["dagger", "ruby", "gold coin", "gold coin", "ruby"]
+dragon_loot_3 = ["broken dagger", "gold coin", "ruby", "vorpal sword"]
 
-initInv_1 = {"gold coin": 42, "rope": 1}
-initInv_2 = {"rope": 2, "gold coin": 1337}
-initInv_3 = {"vorpal sword": 1, "rope": 3, "gold coin": 42}
-
-dragonLoot_1 = ["gold coin", "dagger", "gold coin", "gold coin", "ruby"]
-dragonLoot_2 = ["dagger", "ruby", "gold coin", "gold coin", "ruby"]
-dragonLoot_3 = ["broken dagger", "gold coin", "ruby", "vorpal sword"]
-
-finalInv = addToInventory(initInv_1, dragonLoot_2)
-displayInventory(finalInv)
+finalInv = add_to_inventory(starting_inventory_1, dragon_loot_2)
+display_inventory(finalInv)
